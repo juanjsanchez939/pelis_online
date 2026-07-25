1673ef857de4560e2917b425f3e7f8be7f59e0c9
juanjsanchez939 <juanjsanchezgarcia939@gmail.com>
2026-07-25 16:04:26 -0300

    Configuracion local: Docker MongoDB, mobile localhost, backend local

--- mobile/src/config.js
+++ mobile/src/config.js
@@ -1,2 +1,9 @@
-export const API_BASE_URL = 'https://pelis-online-api.onrender.com';
+import { Platform } from 'react-native';
+
+export const API_BASE_URL = Platform.select({
+  android: 'http://10.0.2.2:3000',
+  ios: 'http://localhost:3000',
+  default: 'http://localhost:3000',
+});
+
 export const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
